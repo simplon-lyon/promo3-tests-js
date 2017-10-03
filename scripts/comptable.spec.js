9ad393017a54534da1d8a6c2b8bc684c6758dc87
Jean <demeljean@hotmail.fr>
2017-10-03 12:32:51 +0200

    tested with spy

--- scripts/comptable.spec.js
+++ scripts/comptable.spec.js
@@ -1,5 +1,6 @@
 describe('class Comptable', ()=> {
     let comptable;
+    let spyCalcul;
 
     beforeEach(() => {
         comptable = new Comptable();
@@ -9,22 +10,30 @@ describe('class Comptable', ()=> {
         à tester, mais dont les retours sont contrôlés
         par le test.
         */
-        let mockCalculette = {
-            calcul:function() {
-                return 4;
-            }
-        };
-        comptable.calculette = mockCalculette;
+        // let mockCalculette = {
+        //     calcul:function() {
+        //         return 4;
+        //     }
+        // };
+        // comptable.calculette = mockCalculette;
+        spyCalcul = jasmine.createSpy('calcul() spy');
+        comptable.calculette.calcul = spyCalcul;
     });
 
     it('should return "en faillite"', () => {
+        spyCalcul.and.returnValue(4);
+
         expect(comptable.getBenefices(100, 5, 100))
         .toBe('en faillite');
+
+        expect(comptable.calculette.calcul).toHaveBeenCalledWith(100,100,'*');
     });
 
     it('should return 4', () => {
+        spyCalcul.and.returnValue(4);
         expect(comptable.getBenefices(100, 3, 100))
         .toBe(4);
+        expect(spyCalcul).toHaveBeenCalledTimes(2);
     });
 
 });
